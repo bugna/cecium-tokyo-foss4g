f79b7117681c795630f242343094f75ddc50b842
bugna <uedats@gmail.com>
2015-10-09 12:39:19 +0900

    コード修正

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,10 +1,10 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
 //視点の変更
-viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(139.76,35.67, 15000.0)});
+viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(139.620395,35.321708, 15000.0)});
 
 //kml読み込み
-viewer.dataSources.add(Cesium.KmlDataSource.load('http://bugna.github.io/cecium-tokyo-foss4g/cesium-starter-app-master/Source/tokyo_foss4g.kml'));
+//viewer.dataSources.add(Cesium.KmlDataSource.load('http://bugna.github.io/cecium-tokyo-foss4g/cesium-starter-app-master/Source/tokyo_foss4g.kml'));
 
 //自宅
 //ポイントを追加
