6998a3200c37681faaff5bed1772f9c1adcc22d3
bugna <uedats@gmail.com>
2015-10-09 12:16:14 +0900

    コード間違い

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -4,5 +4,5 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(139.76,35.67, 15000.0)});
 
 //kml読み込み
-viewer.dataSources.add(Cesium.KmlDataSource.load('http://bugna.github.io/cecium-tokyo-foss4g/Source/tokyo_foss4g.kml'));
+viewer.dataSources.add(Cesium.KmlDataSource.load('http://bugna.github.io/cecium-tokyo-foss4g/cesium-starter-app-master/Source/tokyo_foss4g.kml'));
 
